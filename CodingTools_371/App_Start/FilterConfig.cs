68bb80fa35015f0c049fa8bd710a12e739cb85be
Steven Doolan <doolansr@rose-hulman.edu>
2015-10-20 17:12:14 -0400

    debug azure security issues

---
+++ CodingTools_371/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CodingTools_371
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CodingTools_371/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodingTools_371.MvcApplication" Language="C#" %>
