0dbbad79d8da5f9cf79de7f28943fdbbb88ae14d
Steven Doolan <doolansr@rose-hulman.edu>
2015-11-06 02:14:48 -0500

    Base Tool View Fixed, Mocked data

--- CodingTools_371/Scripts/Tool.js
+++ CodingTools_371/Scripts/Tool.js
@@ -1,18 +1,7 @@
-﻿
-
-
-window.KO_MODEL;
+﻿window.KO_MODEL;
 $(document).ready(function () {
-    var KO_MODEL = ViewToolsMain({
-        "filters": [
-                    {
-                        "name": 'Sites',
-                        "options": JSONSITEINFO
-                    }
-        ], "siteData": JSONSITEINFO
-
-    });
-    ko.applyBindings(KO_MODEL);
+    window.KO_MODEL = ViewToolsMain(JSONSITEINFO);
+    ko.applyBindings(window.KO_MODEL);
 
 });
 
@@ -20,59 +9,22 @@ $(document).ready(function () {
 
 // --------------------- Models ---------------------------------//
 
-
-var siteInfo = function (data) {
+var Tag = function(data) {
     var self = this;
     self.name = data.name;
-    self.url = data.url;
-    self.img = data.img;
-    self.reviews = data.reviews;
-    self.desc = data.desc;
-    self.tags = data.tags;
 }
 
 var ViewToolsMain = function (data) {
     var self = this;
-    self.siteInfo = ko.observable(new siteInfo(data.siteData));
-    self.imageUrl = data.img
+    self.name = data.name;
+    self.url = data.url;
+    self.imageUrl = data.img;
+    self.reviews = data.reviews;
+    self.desc = data.desc;
+    self.tags = ko.observableArray($.map(data.tags, function (text) { return new Tag(text) }));
 
+    //self.jumbotronCSS = '#d3d3d3 url('+ self.imageUrl+') center center;';
 };
-
-function displayTags() {
-    var tagsArea = document.getElementById('tags');
-    //var output = "";
-    var tagsToAdd = siteInfo().tags;
-    for (var i = 0; i < tagsToAdd.length; i++) {
-        //output = output + tagsToAdd[i];
-        var newSpan = document.createElement('span');
-        newSpan.style.width = '100px';
-        newSpan.style.height = '50px';
-        newSpan.style.border = '1px solid black';
-        newSpan.style.fontSize = '25px';
-        newSpan.style.padding = '5px';
-        newSpan.innerHTML = tagsToAdd[i];
-        tagsArea.appendChild(newSpan);
-
-    }
-}
-
-function settupLink() {
-    var url = siteInfo().url;
-    document.getElementById('link').href = url;
-    //	console.log(url);
-}
-
-function setBackground() {
-    var img = siteInfo().img;
-    document.getElementById('jumbotron').style.backgroundImage = 'url(' + img + ')';
-}
-
-window.onload = function () {
-    displayTags();
-    settupLink();
-};
-
-
 
 
 /**--------------SAMPLE DATA-------------------**/
@@ -80,12 +32,16 @@ var JSONSITEINFO =
 {
     "name": 'Test',
     "url": 'http://www.codeacademy.com',
-    "img": '../../Images/Codeacademy.jpg',
+    "img": '../../Images/Tool/Codeacademy.jpg',
     "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nisi tortor, eleifend a nulla sit amet, euismod cursus felis. Duis tincidunt sodales fermentum. Sed finibus tempus augue fringilla lobortis. Aenean mollis massa dui, id lacinia risus feugiat vitae. Duis tincidunt enim a neque accumsan hendrerit. Donec mollis, odio vitae finibus efficitur, quam diam efficitur diam, ac rhoncus justo est id urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     "reviews": ['review1 ', 'review 2'],
-    "tags": ['9th Grade ', '8th Grade ', 'Laptop ', 'Chromebook ']
-}
-;
+    "tags": [
+            {'name': '8th Grade' },
+            {'name': '9th Grade' },
+            {'name': 'Laptop'},
+            {'name': 'Chromebook'}
+    ]
+};
 
 
 
