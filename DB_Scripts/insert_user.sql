2e71fc295cb9efd023e7a522e25233aa56de2434
Davis Nygren <nygrendr@rose-hulman.edu>
2015-11-09 08:50:00 -0500

    Added two insert sprocs

---
+++ DB_Scripts/insert_user.sql
@@ -0,0 +1,16 @@
+IF OBJECT_ID('insert_user', 'P') IS NOT NULL
+DROP PROCEDURE insert_user
+GO
+
+CREATE PROCEDURE [insert_user]
+	(@Name nvarchar(255),
+	@Email nvarchar(255),
+	@Title nvarchar(255),
+	@Username nvarchar(30))
+AS
+
+INSERT INTO [Users]
+([Name], [Email], [Title], [Username])
+VALUES (@Name, @Email, @Title, @Username)
+
+RETURN 0
